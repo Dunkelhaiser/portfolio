c503dad70e07db4e6437cd7bd2de1edda38d35c3
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2026-04-19 17:48:39 +0300

    feat: add time option

--- apps/gallery/src/lib/utils.ts
+++ apps/gallery/src/lib/utils.ts
@@ -1,11 +1,17 @@
 const secondInMs = 1000;
 
-export const unixToDate = (unixTime: number) => {
+export const unixToDate = (unixTime: number, time = false) => {
     const date = new Date(Number(unixTime) * secondInMs);
 
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
+
+    if (time) {
+        const hours = String(date.getHours()).padStart(2, "0");
+        const minutes = String(date.getMinutes()).padStart(2, "0");
+        return `${day}/${month}/${year} ${hours}:${minutes}`;
+    }
 
     return `${day}/${month}/${year}`;
 };
