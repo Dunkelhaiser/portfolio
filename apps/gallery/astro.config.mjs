3ad85dce6b8d638552d0fdffc1464cf3c1f488b0
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2026-05-03 22:08:49 +0300

    deps: install cloduflare adapter

--- apps/gallery/astro.config.mjs
+++ apps/gallery/astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
+
+import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+    adapter: cloudflare(),
+    output: "server",
     vite: {
         plugins: [tailwindcss()],
     },
-
     integrations: [react()],
 });
